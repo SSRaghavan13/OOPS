ff3361fd84f67ab7cbb2f5d182589247d2c12835
SSRaghavan13 <ssraghavan25@gmail.com>
2020-05-16 10:55:20 -0700

    Right shift LL

--- ElementsProgramming/src/linkedlist/P5RightShift.java
+++ ElementsProgramming/src/linkedlist/P5RightShift.java
@@ -2,42 +2,26 @@ package linkedlist;
 
 public class P5RightShift {
     public static Node<Integer> rightCyclicShift(Node<Integer> head, int k) {
+        if(head == null)
+            return head;
         Node<Integer> dummyHead = new Node<Integer>(0);
         dummyHead.next = head;
         Node<Integer> temp = dummyHead;
-        int counter = k;
+        int counter;
+        int len = 0;
+        while(temp.next != null) {
+            len++;
+            temp = temp.next;
+        }
+        temp.next = head;
+        counter = len - k;
+        temp = dummyHead;
         while(counter-- > 0)
             temp = temp.next;
         Node<Integer> newHead = temp.next;
         temp.next = null;
         dummyHead.next = newHead;
-        while(newHead.next != null)
-            newHead = newHead.next;
-        newHead.next = head;
         return dummyHead.next;
-    }
-
-    public static Node<Integer> cyclicallyRightShiftList(Node<Integer> head, int k) {
-        if(head == null)
-            return head;
-        Node<Integer> tail = head;
-        int n = 1;
-        while(tail.next != null) {
-            ++n;
-            tail = tail.next;
-        }
-        k %= n;
-        if(k == 0)
-            return head;
-        
-        tail.next = head;
-        int stepToNewHead = n - k;
-        Node<Integer> newTail = tail;
-        while(stepToNewHead-- > 0)
-            newTail = newTail.next;
-        Node<Integer> newHead = newTail.next;
-        newTail.next = null;
-        return newHead;
     }
     
     public static void main(String[] args) {
@@ -52,7 +36,5 @@ public class P5RightShift {
 		n4.next = n5;
         n5.next = null;
         Node.print(rightCyclicShift(n1, 3));
-        System.out.println("--------------------------------------------------------");
-        Node.print(cyclicallyRightShiftList(n1, 3));
     }
 }
